979074fa5456d72d2e44af60ad1e81ed7246cb6f
Jose L <jose0909>
2025-07-02 22:46:49 -0500

    initial commit

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Home Screen')
+        ),
+      ),
+      body: Center(
+        child: Text('El home screen aqui'),
+      ),
+    );
+  }
+}
