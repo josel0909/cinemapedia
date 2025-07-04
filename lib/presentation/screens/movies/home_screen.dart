a9fac5e98a1c74c6b24d7fa259e85031ffa02491
Jose L <jose0909>
2025-07-04 15:32:00 -0500

    get movies nowplaying

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -14,7 +16,51 @@ class HomeScreen extends StatelessWidget {
           child: Text('Home Screen')
         ),
       ),
-      body: Placeholder()
+      body: _HomeView()
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.isEmpty) {
+      return Center(
+        child: CircularProgressIndicator()
+      );
+    }
+
+
+    return Scaffold(
+      body: ListView.separated(
+        itemCount: nowPlayingMovies.length,
+        itemBuilder: (BuildContext context, int index) {
+          final movie = nowPlayingMovies[index];
+          return ListTile(
+            leading: Image.network(movie.posterPath),
+            title: Text(movie.title),
+          );
+        }, separatorBuilder: (BuildContext context, int index) { 
+          return SizedBox(height: 10,);
+         },
+      ),
     );
   }
 }
